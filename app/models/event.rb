cf0ad04e2a589d9c85cc2852277eda88486a4f39
Sherry He <xsherryhe@gmail.com>
2022-08-10 20:05:37 -0400

    Refactored Event class methods into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,15 +4,15 @@ class Event < ApplicationRecord
   has_many :event_registrations, foreign_key: "attended_event_id"
   has_many :attendees, through: :event_registrations
 
-  def self.future
+  scope :future, (lambda do
     where('happening_date > ?', Date.current)
    .or(where(happening_date: Date.current).where('happening_time >= ?', Time.current))
    .or(where(happening_date: Date.current).where(happening_time: nil))
    .or(where(happening_date: nil))
-  end
+  end)
 
-  def self.past
+  scope :past, (lambda do
     where('happening_date < ?', Date.current)
    .or(where(happening_date: Date.current).where('happening_time < ?', Time.current))
-  end
+  end)
 end
