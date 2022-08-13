3e9864ae98dd1a1f697ded2e9bf8dbf1eaa7473a
Sherry He <xsherryhe@gmail.com>
2022-08-12 22:08:45 -0400

    Minor edits related to bolding invitations when they are new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,14 @@
 module ApplicationHelper
-  def new_invitations_display
+  def strong_if_condition(condition, &block)
+    if condition
+      content_tag(:strong, capture(&block))
+    else
+      block.call
+    end
+  end
+
+  def new_invitations_string
     count = current_user&.received_invitations&.not_viewed&.count
-    count && count > 0 ? "(#{count} New)" : ''
+    count && count > 0 ? "(#{count} New)" : false
   end
 end
