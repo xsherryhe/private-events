d9b4ac4044243145a4945b99f543819e329a0ee5
Sherry He <xsherryhe@gmail.com>
2022-08-13 23:04:58 -0400

    Began CSS styling for general layout and header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,6 @@ module ApplicationHelper
 
   def new_invitations_string
     count = current_user&.received_invitations&.not_viewed&.count
-    count && count > 0 ? "(#{count} New)" : false
+    count && count > 0 ? "<br />(#{count} New)" : false
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module PrivateEvents
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.autoload_paths << config.root.join('lib/assets')
+    config.autoload_paths << config.root.join('lib', 'assets')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
