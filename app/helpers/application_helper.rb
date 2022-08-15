57bfa9596db0215d8a9cf6ab2477c31f5231c90d
Sherry He <xsherryhe@gmail.com>
2022-08-15 18:43:21 -0400

    Completed CSS styling for invitations#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def strong_if_condition(condition, &block)
-    if condition
-      content_tag(:strong, capture(&block))
-    else
-      block.call
-    end
-  end
-
   def new_invitations_string
     count = current_user&.received_invitations&.not_viewed&.count
     count && count > 0 ? "<br />(#{count} New)" : false
