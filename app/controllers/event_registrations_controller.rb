9aa8c0b418528859c5291f37f7b55246619277a9
Sherry He <xsherryhe@gmail.com>
2022-08-16 22:16:05 -0400

    Fixed minor typo

--- app/controllers/event_registrations_controller.rb
+++ app/controllers/event_registrations_controller.rb
@@ -29,7 +29,7 @@ class EventRegistrationsController < ApplicationController
                                              .merge(attended_event_id: @event.id))
     begin
       if @event_registration.save
-        flash[:notice] = "Successfully registed for event “#{@event.name}”!"
+        flash[:notice] = "Successfully registered for event “#{@event.name}”!"
         redirect_to @event
       else
         render :new, status: :unprocessable_entity
